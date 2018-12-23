0e4074920625769fbc6b56b118202ad9ed38a5bc
yaw amoabeng <you@example.com>
2018-12-23 15:24:36 -0500

    yaw amoabeng 3772ny exam

--- src/math/problems/FindMissingNumber.java
+++ src/math/problems/FindMissingNumber.java
@@ -4,14 +4,36 @@ package math.problems;
  * Created by mrahman on 04/22/17.
  */
 public class FindMissingNumber {
-
     public static void main(String[] args) {
         /*
          * If n = 10, then array will have 9 elements in the range from 1 to 10.
          * For example {10, 2, 1, 4, 5, 3, 7, 8, 6}. One number will be missing in array (9 in this case).
          * Write java code to find the missing number from the array. Write static helper method to find it.
          */
-         int [] array = new int[]{10, 2, 1, 4, 5, 3, 7, 8, 6};
+        int[] array = new int[]{10, 2, 1, 4, 5, 3, 7, 8, 6};
+        System.out.println(findingMissing, (10));
+    }
+
+    public static int findingMissing(int[] array, int n) {
+
+        int total = 0;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum + < i;
+        }
+        for (int i = 0; i < array.length; i++) {
+            total += i;
+
+        }
+        return (sum - total);
+    }
+}
+
+
+
+
+        }
+        }
 
     }
 }
--- src/string/problems/Anagram.java
+++ src/string/problems/Anagram.java
@@ -1,5 +1,7 @@
 package string.problems;
 
+import java.util.Arrays;
+
 /**
  * Created by mrahman on 04/22/17.
  */
@@ -8,5 +10,10 @@ public class Anagram {
     public static void main(String[] args) {
         //Write a Java Program to check if the two String are Anagram. Two String are called Anagram when there is
         //same character but in different order.For example,"CAT" and "ACT", "ARMY" and "MARY".
-    }
+        String x = "ARMY";
+        String y = "MARY";
+        System.out.println(isAnagram(x, y));
+    public static boolean is Anagram(String str1,String str2){
+        if(str1.length()!)
+        }
 }
